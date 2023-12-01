bac3f9849e923d22e50b070886eb9546d5d04f03
kelvinatta <300ices@gmail.com>
2023-12-01 16:45:06 +0000

    latest

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Answer from '@/components/forms/Answer';
 import AllAnswers from '@/components/shared/AllAnswers';
 import Metric from '@/components/shared/Metric';
-import ParseHTML from '@/components/shared/ParseHTML';
+
 import RenderTag from '@/components/shared/RenderTag';
 import Votes from '@/components/shared/Votes';
 import { getQuestionById } from '@/lib/actions/question.action';
