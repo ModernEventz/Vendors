f959e9ede66035f4b5b9fda4ff4b090c4a0efc55
ModernEventz <eventzmodern@gmail.com>
2024-12-02 17:04:28 +0000

    Update columns.tsx

---
+++ app/(root)/budget/columns.tsx
@@ -0,0 +1,77 @@
+"use client"
+import { ColumnDef ,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+import {bookings } from "@/types/collections";
+import { getTimestamp } from '@/lib/utils'
+    const columns: ColumnDef<bookings>[] = [
+  
+      {
+        id: "select",
+        header: ({ table }) => (
+          <Checkbox
+            checked={
+              table.getIsAllPageRowsSelected() ||
+              (table.getIsSomePageRowsSelected() && "indeterminate")
+            }
+            onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+            aria-label="Select all"
+            className="border-rose-600"
+          />
+        ),
+        cell: ({ row }) => (
+          <Checkbox
+            checked={row.getIsSelected()}
+            onCheckedChange={(value) => row.toggleSelected(!!value)}
+            aria-label="Select row"
+            className="border-rose-600"
+          />
+        ),
+      },
+    
+        {
+          accessorKey: "name",
+          header: "Name",
+        },
+        {
+          accessorKey: "phone_number",
+           header: "Number"
+        },
+        {
+          accessorKey: "message",
+           header: "Message",
+           cell: ({ row }) => {
+            const payment = row.original
+       
+            return (
+              <MessageDetails Message={payment.message} />
+            )
+          },
+        },
+
+        {
+          accessorKey: "date",
+           header: "DateTime",
+           cell: ({ row }) => {
+            const payment = row.original
+       
+            return (
+              <p className='text-sm '>{getTimestamp(new Date(payment.date))}</p>
+            )
+          },
+        },
+        
+       
+
+       
+    ]
+    
+
