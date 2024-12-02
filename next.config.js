dc3362b77f40d068c3ed21b29503ddf1c51c84ca
ModernEventz <eventzmodern@gmail.com>
2024-12-02 14:50:54 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
     mdxRs: true,
     serverComponentsExternalPackages: ['mongoose']
   },
+
+     eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
