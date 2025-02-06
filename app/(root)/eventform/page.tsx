0b746bf93d665c00256069edfd37b5206e3a1c44
ModernEventz <eventzmodern@gmail.com>
2025-02-06 11:36:23 +0000

    align

--- app/(root)/eventform/page.tsx
+++ app/(root)/eventform/page.tsx
@@ -220,7 +220,7 @@ const Page = (props) => {
                 control={form.control}
                 name="preferNotSpecifyPrice"
                 render={({ field }) => (
-                  <FormItem className="flex justify-start">
+                  <FormItem className="grid  grid-cols-2 place-content-start gap-2">
                     <FormControl>
                       <input
                         type="radio"
